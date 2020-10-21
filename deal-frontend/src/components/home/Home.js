37b6451b3f9602acb2d4a3f69b7b30a6db70fa1b
Robert Tyler Young <roberttyleryoung@gmail.com>
2020-10-21 18:02:58 +0100

    add react bootstrap

--- deal-frontend/src/components/home/Home.js
+++ deal-frontend/src/components/home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Alert } from 'react-bootstrap';
 
 const Home = () => {
     return (
         <div>
             <p>Home</p>
+            <Alert variant="dark">
+                Does bootstrap work? Let's find out!
+            </Alert>
         </div>
     )
 }
