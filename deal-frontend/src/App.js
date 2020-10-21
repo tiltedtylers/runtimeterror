792798996954559d036ec375434b7b09e290cf90
Abinaskarki <youbeapro@gmail.com>
2020-10-21 18:40:53 +0100

    Added Login and Signup Page

--- deal-frontend/src/App.js
+++ deal-frontend/src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/home/Home'
+import Login from './components/profile/Login'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Signup from './components/profile/Signup'
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home}></Route>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={Signup} />
         </Switch>
       </Router>
     </div>
---
+++ deal-frontend/src/components/profile/Signup.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel, Form } from "react-bootstrap";
+import "./login.css";
+
+export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email, password);
+  }
+
+  return (
+    <div className="main">
+        <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email">
+          <FormLabel>Email</FormLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block disabled={!validateForm()} type="submit">
+          Signup
+        </Button>
+      </form>
+    </div>
+  );
+}
