b7cc7c151b1cbec06e36151d2c0bc04f9f0a115b
camimanj <60715497+camimanj@users.noreply.github.com>
2020-10-22 19:51:46 +0100

    Average Buy and Sell Price

--- deal-backend/userQueries.py
+++ deal-backend/userQueries.py
@@ -4,8 +4,14 @@ connection = mysql.connector.connect(host='localhost', database='mysql', user='r
                                      password='ppp', auth_plugin='mysql_native_password')
 cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM db_grad_cs_1917.deal")
+#Average Sell Price
+cursor.execute("SELECT instrument_name, AVG(deal_amount) FROM db_grad_cs_1917.deal INNER JOIN db_grad_cs_1917.instrument ON deal.deal_instrument_id=instrument.instrument_id WHERE deal_type='S' AND deal_time BETWEEN '2017-07-28T17:06:29.955' AND '2017-07-28T17:06:30.049' GROUP BY deal_instrument_id;")
+for avgSellPrice in cursor:
+    print(avgSellPrice)
 
-for deal in cursor:
-    print(deal)
+#Average Buy Price
+cursor.execute("SELECT instrument_name, AVG(deal_amount) FROM db_grad_cs_1917.deal INNER JOIN db_grad_cs_1917.instrument ON deal.deal_instrument_id=instrument.instrument_id WHERE deal_type='B' AND deal_time BETWEEN '2017-07-28T17:06:29.955' AND '2017-07-28T17:06:30.049' GROUP BY deal_instrument_id;")
+for avgBuyPrice in cursor:
+    print(avgBuyPrice)
+
 
