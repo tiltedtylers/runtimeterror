7437b893b706f77ef3c8f694a296a8e52f0c73b6
camimanj <60715497+camimanj@users.noreply.github.com>
2020-10-22 20:12:12 +0100

    Average Buy and Sell Price Functionality

--- deal-backend/userQueries.py
+++ deal-backend/userQueries.py
@@ -5,13 +5,14 @@ connection = mysql.connector.connect(host='localhost', database='mysql', user='r
 cursor = connection.cursor()
 
 #Average Sell Price
+avgSellBuyDict = {} #Dictionary where instrument name is the key, value is an array with Avg Sell Price at index 0 and avg Buy Price at index 1
 cursor.execute("SELECT instrument_name, AVG(deal_amount) FROM db_grad_cs_1917.deal INNER JOIN db_grad_cs_1917.instrument ON deal.deal_instrument_id=instrument.instrument_id WHERE deal_type='S' AND deal_time BETWEEN '2017-07-28T17:06:29.955' AND '2017-07-28T17:06:30.049' GROUP BY deal_instrument_id;")
 for avgSellPrice in cursor:
-    print(avgSellPrice)
+    avgSellBuyDict[avgSellPrice[0]] = {"avgSellPrice": avgSellPrice[1]}
 
 #Average Buy Price
 cursor.execute("SELECT instrument_name, AVG(deal_amount) FROM db_grad_cs_1917.deal INNER JOIN db_grad_cs_1917.instrument ON deal.deal_instrument_id=instrument.instrument_id WHERE deal_type='B' AND deal_time BETWEEN '2017-07-28T17:06:29.955' AND '2017-07-28T17:06:30.049' GROUP BY deal_instrument_id;")
 for avgBuyPrice in cursor:
-    print(avgBuyPrice)
+    avgSellBuyDict[avgBuyPrice[0]]["avgBuyPrice"] = avgBuyPrice[1]
 
-
+print("done")
