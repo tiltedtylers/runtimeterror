7fafd3e9dcd2899347e86fd711c85cb9cc53642c
Alvores <gzingle@gmail.com>
2020-10-23 16:33:45 +0100

    Adding test for average buy sell

--- test/testUserQueries.py
+++ test/testUserQueries.py
@@ -1,9 +1,10 @@
 from deal_backend.userQueries import calculate_avg_buy_sell_price
 
-def test_calculate_avg_buy_sell_price(start_date = '2017-07-28T17:06:29.955', end_date = '2017-07-28T17:06:30.049'):
+def test_calculate_avg_buy_sell_price():
     testInstrument = "Astronomica"
     expected = {"avgSellPrice": 3777.000000, "avgBuyPrice": 3407.785000}
     result = calculate_avg_buy_sell_price(start_date = '2017-07-28T17:06:29.955', end_date = '2017-07-28T17:06:30.049')
-    assert expected == result.get(testInstrument)
-
-
+    print(expected == result.get(testInstrument))
+    print(expected)
+    print(result)
+test_calculate_avg_buy_sell_price()
